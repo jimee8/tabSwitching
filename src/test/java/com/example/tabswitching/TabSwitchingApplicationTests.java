60eadadb293d3e3f2dd1a1119b65c15b27b40e1b
jimee8 <jimeelam@gmail.com>
2026-06-23 20:47:03 -0600

    Initial commit

---
+++ src/test/java/com/example/tabswitching/TabSwitchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabswitching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabSwitchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
